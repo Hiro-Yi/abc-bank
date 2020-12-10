7f92a9542c9ad37c6ec458b26793b3a982164608
Ryuk500 <ShinigamiRyuk@tuta.io>
2020-12-10 16:42:22 +0530

    new

--- modules/database.py
+++ modules/database.py
@@ -5,16 +5,8 @@ from getpass import getpass
 
 try:
     util.clear()
-    print("===================================================")
-    db_user = input(" Enter your Mysql username: ")
-    print("===================================================")
-    db_password = getpass(" Enter your Mysql password: ")
-    print("===================================================")
-    input("\nPress Any Key to Continue ...")
-    print("===================================================")
-
     connection = mysql.connector.connect(
-        host='127.0.0.1', user=db_user, password=db_password, database="abc_bank")
+        host='127.0.0.1', user="root", password="root", database="abc_bank")
     db = connection.cursor()
 
 except:
