d7ab74ca60bfbc1aaf464ffb4da4cb2bd2e82962
Ryuk500 <ShinigamiRyuk@tuta.io>
2020-12-10 18:53:18 +0530

    fix all bug of profile

--- modules/profile.py
+++ modules/profile.py
@@ -40,17 +40,19 @@ def profile(userId):
                 else:
                     db.execute("UPDATE users SET username = %s where username = %s ",(input_user,f"{user[1]}"))
 
-            if answer == 2 :
+            elif answer == 2 :
                 print("Enter your Gender (M/F)")
                 input_user = input("> ")
                 input_user = input_user.upper()
                 if input_user == "M" or "F" :
                     db.execute("UPDATE users SET gender = %s where username = %s ",(input_user,f"{user[1]}"))
+                    connection.commit()
+                
                 else:
                     print("\u001b[31;1mPlease enter a valid Gender !!")
                     input("\nPress Any Key to Continue ...\u001b[34;1m")
                     
-            if answer == 3 :
+            elif answer == 3 :
                 print("Enter your Age ")
                 input_user = int(input("> "))
                 if input_user < 13:
@@ -63,14 +65,16 @@ def profile(userId):
 
                 else:
                     db.execute("UPDATE users SET age = %s where username = %s ",(input_user,f"{user[1]}"))
+                    connection.commit()
                     
             
-            if answer == 4 :
+            elif answer == 4 :
                 print("Enter your City")
                 input_user = input("> ")
                 db.execute("UPDATE users SET city = %s where username = %s ",(input_user,f"{user[1]}"))
+                connection.commit()
 
-            if answer == 5 :
+            elif answer == 5 :
                 print("Enter your Phone No ")
                 input_user = int(input("> "))
                 try:
@@ -78,6 +82,7 @@ def profile(userId):
                         db.execute(
                             "SELECT * FROM users WHERE phone = %s", (input_user,))
                         result = db.fetchone()
+                        connection.commit()
 
                         if result != None:
                             print("\u001b[31;1mPhone number is already registered !!")
@@ -85,6 +90,7 @@ def profile(userId):
 
                         else:
                             db.execute("UPDATE users SET phone = %s where username = %s ",(input_user,f"{user[1]}"))
+                            connection.commit()
 
                     else:
                         print("\u001b[31;1mPlease enter a valid phone number !!")
@@ -101,6 +107,7 @@ def profile(userId):
                 db.execute(
                     "select * from users where username = %s and password = SHA1(%s)", ({user[1]}, password))
                 result = db.fetchone()
+                
 
                 if result != None:
                     while True:
@@ -110,6 +117,9 @@ def profile(userId):
                         passwordSec = getpass("> ")
                         if passwordOne == passwordSec :
                             print("\u001b[32mPassword changed .\u001b[34;1m")
+                            db.execute("select * from users where username = %s and password = SHA1(%s)", ({user[1]}, passwordOne))
+                            connection.commit()
+                
                             pass
                         else:
                             print("\u001b[31;1mPassword dosen`t match !!")
