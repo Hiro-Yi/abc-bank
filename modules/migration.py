e6b6774d207bbe75550b477f5448c8da68c1c848
Siltaz <Siltaz001@gmail.com>
2020-12-10 21:02:24 +0530

    Mini Statement added & db auth mechanism changed to 'mysql_native_password'

--- modules/migration.py
+++ modules/migration.py
@@ -13,14 +13,13 @@ print("===================================================")
 
 
 try:
-    conn = mysql.connector.connect(
-        host='127.0.0.1', user=db_user, password=db_password)
+    conn = mysql.connector.connect(host='127.0.0.1', user=db_user, password=db_password, auth_plugin="mysql_native_password")
     myCursor = conn.cursor()
     myCursor.execute("drop database if exists abc_bank")
     myCursor.execute("create database abc_bank")
     myCursor.execute("use abc_bank")
     myCursor.execute("create table users(id int(11) NOT NULL Primary Key auto_increment, username varchar(64) NOT NULL UNIQUE, first_name varchar(64) NOT NULL,last_name varchar(64) NOT NULL,gender enum('M','F') NOT NULL,age int(3) NOT NULL,phone varchar(10) NOT NULL Unique Key,city varchar(255) NOT NULL,password varchar(100) NOT NULL)")
-    myCursor.execute("create table transactions(id int(11) NOT NULL Primary Key auto_increment, user_id int(11) NOT NULL, type enum('WITHDRAW','DEPOSIT') NOT NULL, amount double(16,2) NOT NULL, FOREIGN KEY (user_id) REFERENCES users(id))")
+    myCursor.execute("create table transactions(id int(11) NOT NULL Primary Key auto_increment, user_id int(11) NOT NULL, type enum('WITHDRAW','DEPOSIT') NOT NULL, amount double(16,2) NOT NULL, timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP, FOREIGN KEY (user_id) REFERENCES users(id))")
 
 except:
     print("Invalid Mysql username/password !!")
